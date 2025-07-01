bdf16bdf0f8f0c411b972472e250a486698ead1b
anus26 <anusraza268gmail.com>
2025-07-01 13:05:23 -0700

    set components

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskForm = () => {
+  return (
+    <div>Taskform</div>
+  )
+}
+
+export default TaskForm
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div>L</div>
+  )
+}
+
+export default TaskList
