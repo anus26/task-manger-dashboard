8c7257905a8b3b8eb79ad776bc4afe3a70bf8b9b
anus26 <anusraza268gmail.com>
2025-07-01 13:00:10 -0700

    task-manager projects setup

---
+++ task-manager/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='font-semibold text-xl bg-black text-white '>Task Manager Dashboard</h1>
+    </>
+  )
+}
+
+export default App
